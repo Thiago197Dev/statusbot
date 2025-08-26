e7fcf08dabbfccaaaebaaf347c23d4df0be8e9ba
Thiago197Dev <hein.denis2000@gmail.com>
2025-08-26 06:47:42 +0200

    New adjustments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => res.send("Bot läuft!"));
+app.listen(PORT, () => console.log(`Webserver läuft auf Port ${PORT}`));
+
+
 require("dotenv").config(); // .env einlesen
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 
